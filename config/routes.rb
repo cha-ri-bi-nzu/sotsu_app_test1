ee71f7ec9f0b1449dc793db61ac5d0580facd792
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-05 21:21:48 +0900

    [add]deviseの前準備
    deviseとletter_openerのルーティング等初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'blogs#index'
   resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
