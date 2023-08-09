4d9e32200b05060dd7ab9da78fd3f98bbab0bc17
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-09 13:30:34 +0900

    [add]shiftテーブル定義
    shiftテーブルのmigrationファイル編集

--- db/migrate/20230809041636_create_shifts.rb
+++ db/migrate/20230809041636_create_shifts.rb
@@ -2,11 +2,15 @@ class CreateShifts < ActiveRecord::Migration[6.1]
   def change
     create_table :shifts do |t|
       t.date :working_day
-      t.references :user, null: false, foreign_key: true
       t.integer :working_status_id
       t.time :working_time
+      t.time :leaving_time
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :shifts, :working_day
+    add_index :shifts, :working_status_id
   end
 end
