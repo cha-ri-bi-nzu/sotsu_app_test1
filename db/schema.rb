42de7a9f5ab9b4e0844163f25738ef08113d665a
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-09 13:31:14 +0900

    [add]shiftテーブル作成
    migrate実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_06_051319) do
+ActiveRecord::Schema.define(version: 2023_08_09_041636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 2023_08_06_051319) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.date "working_day"
+    t.integer "working_status_id"
+    t.time "working_time"
+    t.time "leaving_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shifts_on_user_id"
+    t.index ["working_day"], name: "index_shifts_on_working_day"
+    t.index ["working_status_id"], name: "index_shifts_on_working_status_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +58,5 @@ ActiveRecord::Schema.define(version: 2023_08_06_051319) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shifts", "users"
 end
