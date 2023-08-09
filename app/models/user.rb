f910a8c15a35992d124d581a3c52119e33e52dbc
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-09 15:22:54 +0900

    [add]アソシエーション完了
    users・groupsに対する各テーブルのアソシエーションをmodelにて定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  has_many :sesired_holidays
+  has_many :shifts
+  has_many :groupings, dependent: :destroy
+  has_many :groups, through: :groupings, source: :group
 end
